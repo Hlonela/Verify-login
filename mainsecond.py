bcbf8728776c2bb24c65b04ba952c532cd93d40a
Hlonela Mabhani <82158647+Hlonela@users.noreply.github.com>
2021-06-08 22:43:37 +0200

    Update mainsecond.py

--- mainsecond.py
+++ mainsecond.py
@@ -6,8 +6,12 @@ root.geometry("500x400")
 root.config(bg="Pink")
 root.resizable(height=False, width=False)
 
+def button_exit ():
+        import sys
+        sys.exit()
 
-button1 = Button(root, text="Welcome To Ticket Sales", fg="red", borderwidth=25, bg="yellow", font=15)
+
+button1 = Button(root, text="Thank You", fg="red", borderwidth=25, bg="yellow", font=15, command=button_exit)
 button1.place(x=100, y=100)
 
 root.mainloop()
